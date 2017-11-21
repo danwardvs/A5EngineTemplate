658afb5580d90c3cc2ea884cd20cf324af5c7fe7
Danny Van Stemp <danwardvs@adsgames.net>
2017-11-21 18:23:24 -0500

    FRICKING saving projects in Codeblocks

--- src/main.cpp
+++ src/main.cpp
@@ -132,7 +132,7 @@ void setup(){
   al_start_timer(timer);
 
   // Window title
-  al_set_window_title(display,"Jumping Jimothy");
+  al_set_window_title(display,"Allegro 5 Engine Template");
 
   std::cout<<" Sucesss.\n";
 
